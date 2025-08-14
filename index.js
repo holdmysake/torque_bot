8a2f10e215f37f848830024ab66b13688b0d0477
sahdan17 <sahdan.wira.17@gmail.com>
2025-08-14 14:16:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ let sock
     sock = await startBot()
 })()
 
-app.listen(1717, '127.0.0.1', () => {
-    console.log('Server is running on http://127.0.0.1:1717')
+app.listen(1717, () => {
+    console.log('Server is running on port 1717')
 })
+
+// app.listen(1717, '127.0.0.1', () => {
+//     console.log('Server is running on http://127.0.0.1:1717')
+// })
