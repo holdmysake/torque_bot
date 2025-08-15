48e69f378e5389896a4ec8eebcd26a834a66dd89
sahdan17 <sahdan.wira.17@gmail.com>
2025-08-15 09:05:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,14 +10,19 @@ app.use(express.json())
 
 const server = http.createServer(app)
 const io = new Server(server, {
-    cors: { origin: '*' }
+    cors: { origin: '*' },
+    path: '/ws'
 })
 
 startBot(io)
 
-app.listen(1717, () => {
+server.listen(1717, () => {
     console.log('Server is running on port 1717')
 })
+
+// app.listen(1717, () => {
+//     console.log('Server is running on port 1717')
+// })
 
 // app.listen(1717, '127.0.0.1', () => {
 //     console.log('Server is running on http://127.0.0.1:1717')
