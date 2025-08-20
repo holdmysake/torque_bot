fdbf522c7990fdd5562a277df46c94d7b0a31d68
sahdan17 <sahdan.wira.17@gmail.com>
2025-08-20 08:04:54 +0700

    Update bot.route.js

--- bot.route.js
+++ bot.route.js
@@ -8,46 +8,4 @@ router.post('/stop', async (req, res) => {
     res.json({ message: 'Bot disconnected' })
 })
 
-router.post('/code', (req, res) => {
-    try {
-        const { code } = req.body
-        if (!code) {
-            return res.status(400).json({ error: 'code is required' })
-        }
-
-        const formatted = formatCode(code, 4)
-        return res.json({ formatted })
-    } catch (err) {
-        return res.status(500).json({ error: err.message })
-    }
-})
-
-function formatCode(body, spaces = 4) {
-    const spaceStr = ' '.repeat(spaces)
-    let indent = 0
-    let result = ''
-
-    for (let i = 0; i < body.length; i++) {
-        const char = body[i]
-
-        if (char === '}' || char === ']') {
-            indent = Math.max(0, indent - 1)
-            result += '\n' + spaceStr.repeat(indent) + char
-        }
-        else if (char === '{' || char === '[') {
-            result += char
-            indent++
-            result += '\n' + spaceStr.repeat(indent)
-        }
-        else if (char === '\n') {
-            result += '\n' + spaceStr.repeat(indent)
-        }
-        else {
-            result += char
-        }
-    }
-
-    return result
-}
-
 export default router
